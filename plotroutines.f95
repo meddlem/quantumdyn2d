34c0299204617b09a7057b004b31c4eaacd642f9
meddlem <mprebergen@gmail.com>
2015-04-29 19:42:59 +0200

    fixed plot ratio

--- plotroutines.f95
+++ plotroutines.f95
@@ -20,6 +20,7 @@ contains
       write(10,*) '2 "#0090ff", 3 "#0fffee", 4 "#90ff70", 5 "#ffee00",\' 
       write(10,*) '6 "#ff7000", 7 "#ee0000", 8 "#7f0000")'
       write(10,*) 'set pm3d'
+      write(10,*) 'set size ratio 0.5'
       write(10,*) 'set hidden3d'
       write(10,*) 'set xrange [0:',Lx,']'
       write(10,*) 'set yrange [0:',Ly,']'

2cfe2faecc509383cb0ac5ce6185f5da3af43e7b
meddlem <mprebergen@gmail.com>
2015-05-04 22:57:32 +0200

    cleaned up

--- main.f95
+++ main.f95
@@ -3,31 +3,30 @@ program main
   use structures 
   use initialize
   use simulation
-  use plotroutines
   use io
   implicit none
 
   complex(dp), allocatable :: psi(:,:), Ax(:,:,:), Ay(:,:,:)
-  real(dp), allocatable    :: x(:,:), y(:,:), V(:,:)
+  real(dp), allocatable    :: x(:,:), y(:,:)
   type(modl_par)           :: Q
 
   ! initialize model parameters
   call init_param(Q)
   
   ! allocate arrays
-  allocate(psi(Q%Mx,Q%My), x(Q%Mx,Q%My), y(Q%Mx,Q%My), V(Q%Mx,Q%My), &
-    Ax(3,Q%Mx,Q%My), Ay(3,Q%My,Q%Mx))
+  allocate(psi(Q%Mx,Q%My), x(Q%Mx,Q%My), y(Q%Mx,Q%My), Ax(3,Q%Mx,Q%My), &
+    Ay(3,Q%My,Q%Mx))
   
-  ! initialize simulation
+  ! user input
   call get_usr_args(Q)
   call user_in(Q)
+  
+  ! initialize model 
   call init_ops(Ax, Ay, Q) 
   call init_wavef(psi, x, y, Q)
-  call animate_plot(Q)
 
-  call run_sim(psi, V, x, y, Ax, Ay, Q)
+  ! run simulation
+  call run_sim(psi, x, y, Ax, Ay, Q)
   
-  call close_plot()
-
-  deallocate(psi, x, y, V, Ax, Ay)
+  deallocate(psi, x, y, Ax, Ay)
 end program

1e66ce648a9efdb674ba1909539b304b38729470
meddlem <mprebergen@gmail.com>
2015-05-10 20:38:44 +0200

    final v

--- plotroutines.f95
+++ plotroutines.f95
@@ -18,7 +18,7 @@ contains
     
     ! create a gnuplot command file
     open(10,access = 'sequential',file = 'matplot.plt')
-      write(10,*) 'set term x11'
+      ! write(10,*) 'set term x11' ! uncomment line in case of errors
       write(10,*) 'set palette defined ( 0 "#000090", 1 "#000fff",\' 
       write(10,*) '2 "#0090ff", 3 "#0fffee", 4 "#90ff70", 5 "#ffee00",\' 
       write(10,*) '6 "#ff7000", 7 "#ee0000", 8 "#7f0000")'
